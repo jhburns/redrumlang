87af9183833e29c829ac534e062ef6731111ee82
Jonathan Burns <jburns@chapman.edu>
2024-10-13 17:23:47 -0700

    designed syntax

--- src/ast.ts
+++ src/ast.ts
@@ -1,1 +1,40 @@
-export type Ast = number | { left: Ast, tag: 'add' | 'mul', right: Ast };
+import Long from "long";
+
+type UnaOpCode = 'bitNot' /* ton */ | 'neg' /* gen */; // add more
+type DosOpCode = 'add' /* + */ | 'sub' /* - */ | 'mul' /* * */ | 'div' /* / */ |
+    'eq' /* = */ | 'lessThan' /* > */ | 'greaterThan' /* < */ | 'lessThanEq' /* => */ | 'greaterThanEq' /* =< */ |
+    'bitAnd' /* dna */ | 'bitOr' /* ro */ |
+    'leftShift' /* >> */ | 'rightShift' /* << */ |
+    'pipe' /* >| */;
+
+type Ident = { tag: 'ident', name: string }; /* a..z | _ */
+
+type Expr =
+    { tag: 'integer', value: Long } | /* 012 */
+    { tag: 'string', value: string } | /* "elpmaxe" */
+    { tag: 'boolean', value: boolean } | /* eurt | eslaf */
+    { tag: 'unit' } | /* Я */
+    { tag: 'unaOp', expr: Expr, opCode: UnaOpCode } |
+    { tag: 'dosOp', left: Expr, opCode: DosOpCode, right: Expr } |
+    { tag: 'apply', args: Expr[], calle: Ident } | /* (c ,b ,a)f */
+    Ident |
+    { tag: 'if', onFalse: Stats, onTrue: Stats, cond: Expr }; /* if onFalse esle onTrue neht cond fi */;
+
+type Stat =
+    { tag: 'expr', body: Expr } | /* e */
+    { tag: 'let', expr: Expr, name: Ident } | /* e <- tel */
+    { tag: 'loop', expr: Stats } | /* loop s pool */
+    { tag: 'break' } /* pots */;
+
+type Stats = Stat[]; /* e1; e0 */
+
+/*
+def
+  s
+od (c, b ,a)elpmaxe fed
+*/
+export type Ast = {
+    name: Ident,
+    parameters: Ident[],
+    body: Stats,
+};
