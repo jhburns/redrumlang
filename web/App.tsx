dc6075e67e7d75ca58461180d358f141d5f356e9
Jonathan Burns <jburns@chapman.edu>
2024-10-26 18:29:36 -0700

    Adding control elements

--- web/App.tsx
+++ web/App.tsx
@@ -1,9 +1,11 @@
+import Control from '/web/part/Control';
 import CodeEditor from '/web/part/CodeEditor';
 import Output from '/web/part/Output';
 
 export default function App() {
     return <main className='main-box'>
         <h1>RedЯum Lang</h1>
+        <Control />
         <CodeEditor />
         <Output />
     </main>;
---
+++ web/part/Control.tsx
@@ -0,0 +1,13 @@
+
+export default function Control() {
+    return <div>
+        <button>Execute</button>
+        <button>Я</button>
+
+        <select name="examples" id="examples">
+            <option value="helloWorld">Hello World</option>
+            <option value="fib">Fibonacci</option>
+            <option value="tutorial">Tutorial</option>
+        </select>
+    </div>
+} 
